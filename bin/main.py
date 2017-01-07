2f5d1608ed0cd7dc9ad1930108d929a01c768a1b
Tetsuo Koyama <tkoyama010@gmail.com>
2017-01-07 20:03:24 +0900

    変換関数を確認

--- bin/main.py
+++ bin/main.py
@@ -5,4 +5,5 @@ config = ConfigParser.SafeConfigParser()
 config.read('./config.ini')
 
 name =  config.get('fruit', 'name')
-price =  float(config.get('fruit', 'price'))
+price =  np.float64(config.get('fruit', 'price'))
+print price
